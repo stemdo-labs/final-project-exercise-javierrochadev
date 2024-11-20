efe75ceabb5f74fc21773ebf85bf534377a8d419
javierrochadev <jrocha@stemdo.io>
2024-11-20 11:29:49 +0100

    Create init.sh

---
+++ scripts/init.sh
@@ -0,0 +1,5 @@
+$ mkdir actions-runner && cd actions-runner
+$ curl -o actions-runner-linux-x64-2.320.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.320.0/actions-runner-linux-x64-2.320.0.tar.gz
+$ echo "93ac1b7ce743ee85b5d386f5c1787385ef07b3d7c728ff66ce0d3813d5f46900  actions-runner-linux-x64-2.320.0.tar.gz" | shasum -a 256 -c
+$ tar xzf ./actions-runner-linux-x64-2.320.0.tar.gz
+
